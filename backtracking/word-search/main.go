e63f81c5b13c1452921acff5ca989e6ec774b27f
EvansChang <85141291@qq.com>
2023-12-22 09:24:43 +0800

    update word search

--- backtracking/word-search/main.go
+++ backtracking/word-search/main.go
@@ -4,51 +4,45 @@ import "fmt"
 
 // https://leetcode.cn/problems/word-search
 func main() {
-	fmt.Println(exist([][]byte{{'A'}}, "A"))
+	fmt.Println(exist([][]byte{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}}, "ABCCED"))
 }
 
 var directs = [][]int{{0, 1}, {1, 0}, {-1, 0}, {0, -1}}
 
 func exist(board [][]byte, word string) bool {
-	m, n := len(board), len(board[0])
+	m, n, wordLen := len(board), len(board[0]), len(word)
 	if m*n < len(word) {
 		return false
 	}
 
-	var (
-		backTrack func(i, j int, wordIndex int) bool
-		find      bool
-	)
+	var backTrack func(i, j int, wordIndex int) bool
 
 	backTrack = func(i, j int, wordIndex int) bool {
-		if i >= m || j >= n || i < 0 || j < 0 || board[i][j] == ' ' {
-			return false
+		if wordIndex == wordLen {
+			return true
 		}
-		if board[i][j] != word[wordIndex] {
+		if i >= m || j >= n || i < 0 || j < 0 {
 			return false
 		}
 		tmp := board[i][j]
-		board[i][j] = ' '
-		if wordIndex == len(word)-1 {
-			find = true
-			return true
-		}
-		for _, direct := range directs {
-			if backTrack(i+direct[0], j+direct[1], wordIndex+1) {
-				return true
+		if tmp == word[wordIndex] {
+			board[i][j] = '-'
+			res := false
+			for _, direct := range directs {
+				res = res || backTrack(i+direct[0], j+direct[1], wordIndex+1)
 			}
+			board[i][j] = tmp
+			return res
+		} else {
+			return false
 		}
-		board[i][j] = tmp
-		return false
 	}
 	for i := 0; i < m; i++ {
 		for j := 0; j < n; j++ {
-			if find {
-				return find
+			if backTrack(i, j, 0) {
+				return true
 			}
-			backTrack(i, j, 0)
 		}
 	}
-
-	return find
+	return false
 }
